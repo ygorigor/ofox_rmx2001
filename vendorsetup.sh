e11a638f7e1723129e815f0aeea7be81a1211675
Yillié <yilliee@protonmail.com>
2021-07-22 10:39:03 +0500

    vendorsetup : Add a custom pic for maintainer's about section

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -89,6 +89,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	# run a process after formatting data to work-around MTP issues
 	export OF_RUN_POST_FORMAT_PROCESS=1
 
+	# Custom pic for the maintainer's info in about section
+	export OF_MAINTAINER_AVATAR="$PWD/device/realme/RMX2001/maintainer.png"
+
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
 	  export | grep "FOX" >> $FOX_BUILD_LOG_FILE
