15f740bb8e882c061449973ec9a7246562bce1db
Yilliee <m.taha6f@gmail.com>
2021-08-02 18:17:03 +0500

    Initial Bringup for RMX2001
    This tree is an (edited version of / heavily based on) Samar's twrp tree : http://github.com/TeamWin/android_device_realme_RMX2001.git
    
    The main purpose of this tree is to remove all the extra stuff + extra commit histories of RMX2020 or other old trees that aren't being
    used anymore (have been reverted or shouldn't be in this tree). All that old comit history like 'Update BoardConfig' or RMX2020 twrp ->
    pbrp -> RMX2001 was making it difficult to trace back things.
    I'm trying to add all the authorships properly but incase I miss any (be it your own or of someone else) please let me know.
    
    Co-authored-by: TheMalachite <eliasgheeraert@gmail.com>
    Co-authored-by: SamarV-121 <samarvispute121@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,132 @@
+#
+#       This file is part of the OrangeFox Recovery Project
+#       Copyright (C) 2020-2021 The OrangeFox Recovery Project
+#
+#       OrangeFox is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 3 of the License, or
+#       any later version.
+#
+#       OrangeFox is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       This software is released under GPL version 3 or any later version.
+#       See <http://www.gnu.org/licenses/>.
+#
+#       Please maintain this if you use this script or any part of it
+#
+
+DEVICE_PATH := device/realme/RMX2001
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a55
+
+# Bootloader
+BOARD_VENDOR := realme
+TARGET_SOC := mt6785
+TARGET_BOOTLOADER_BOARD_NAME := mt6785
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6785
+TARGET_BOARD_PLATFORM_GPU := mali-g76
+PLATFORM_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
+
+# Prebuilts
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/10/zImage
+BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/A10/dtb
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/A10/dtbo.img
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+
+# Kernel
+#TARGET_KERNEL_SOURCE :=
+#TARGET_KERNEL_CONFIG :=
+BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x07c08000
+BOARD_KERNEL_TAGS_OFFSET := 0x0bc08000
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_KERNEL_SECOND_OFFSET := 0x00e88000
+BOARD_DTB_OFFSET := 0x0bc08000
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
+BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+
+# Filesystem
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+
+# Dynamic Partition
+BOARD_SUPER_PARTITION_SIZE := 6970933248
+BOARD_SUPER_PARTITION_GROUPS := oppo_dynamic_partitions
+BOARD_OPPO_DYNAMIC_PARTITIONS_SIZE := 6970933248
+BOARD_OPPO_DYNAMIC_PARTITIONS_PARTITION_LIST := \
+        system \
+        vendor \
+        oppo_engineering \
+        oppo_product \
+        common_preload
+
+# System as root
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# Workaround for error copying vendor files to recovery ramdisk
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Metadata
+BOARD_ROOT_EXTRA_FOLDERS += metadata
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+BOARD_USES_METADATA_PARTITION := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 2047
+TW_DEFAULT_BRIGHTNESS := 1200
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_NTFS_3G := true
+TARGET_USES_MKE2FS := true
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+
+# Not sure what's the need of these vars yet
+TW_SCREEN_BLANK_ON_BOOT := true
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+# TW_USE_TOOLBOX := true
---
+++ omni_RMX2001.mk
@@ -0,0 +1,42 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+
+# Inherit our device configuration
+$(call inherit-product, device/realme/RMX2001/device.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_RELEASE_NAME := RMX2001
+PRODUCT_DEVICE := RMX2001
+PRODUCT_NAME := omni_RMX2001
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX2001
+PRODUCT_MANUFACTURER := realme
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
