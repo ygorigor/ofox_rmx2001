dab42637dbdc78258450da3345f5f2cabcae8303
Igor Sorocean <igor_sorocean@hotmail.com>
2024-10-18 13:39:35 +0300

    update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
+# 	Copyright (C) 2021-2024 The OrangeFox Recovery Project
 #
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -35,73 +35,30 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	# Configure some default settings for the build
-	export ALLOW_MISSING_DEPENDENCIES=true
-	export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
-	export OF_VANILLA_BUILD=1
-	export OF_SKIP_ORANGEFOX_PROCESS=1
-	export OF_SUPPORT_OZIP_DECRYPTION=1
-	export FOX_USE_BASH_SHELL=1
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+        export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v27.0.zip
+   	export TW_DEFAULT_LANGUAGE="en"
 	export LC_ALL="C"
-	export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_PATCH_AVB20=1
-        export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
-
-	# Some device specific flags
-	export OF_STATUS_H="102"
-	export OF_STATUS_INDENT_LEFT="155"
-	export OF_STATUS_INDENT_RIGHT="48"
-	export OF_SCREEN_H="2400"
-	export OF_TWRP_COMPATIBILITY_MODE=1
-	export OF_FL_PATH1="/system/flashlight"
-	export OF_FL_PATH2=""
-	export OF_FLASHLIGHT_ENABLE=1
-	export OF_MAINTAINER="Yillié"
-	export OF_TARGET_DEVICES="RMX2001, RMX2002, RMX2003, RMX2151, wasabi"
-	export TARGET_DEVICE_ALT="RMX2001, RMX2151, wasabi"
+ 	export ALLOW_MISSING_DEPENDENCIES=true
+	export FOX_USE_NANO_EDITOR=1
+	export FOX_ENABLE_APP_MANAGER=1
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+        export FOX_DELETE_AROMAFM=1
+        export TARGET_DEVICE_ALT="RMX2001, RMX2151, wasabi"
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+        export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
 
-	# Some Extra Vars
-	export FOX_USE_XZ_UTILS=1
-	export FOX_USE_GREP_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_TAR_BINARY=1
-	export OF_QUICK_BACKUP_LIST="/super;/boot;/dtbo;/vbmeta;/vbmeta_system;/vbmeta_vendor;/data"
-	export OF_USE_TWRP_SAR_DETECT=1
-	export OF_CHECK_OVERWRITE_ATTEMPTS=1
-	export OF_HIDE_NOTCH=1
-	export OF_USE_GREEN_LED=0
-	export OF_SUPPORT_PRE_FLASH_SCRIPT=1
-	export OF_USE_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_ALLOW_DISABLE_NAVBAR=0
-	export FOX_ENABLE_APP_MANAGER=1
-	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1420041600"
+	# R11.1 Settings
+	export FOX_VERSION="R11.1_7"
+	export OF_MAINTAINER="ygorigor"
 
-	#R11
-	export FOX_R11=1
-	export FOX_VERSION="R11.1_1"
-	export FOX_BUILD_TYPE="Stable"
-	export FOX_VARIANT="RUI1.0"
-
-	# maximum permissible splash image size (in kilobytes); do *NOT* increase!
-	export OF_SPLASH_MAX_SIZE="104"
-
-	# run a process after formatting data to work-around MTP issues
-	export OF_RUN_POST_FORMAT_PROCESS=1
-
-	# Custom pic for the maintainer's info in about section
-	export OF_MAINTAINER_AVATAR="$PWD/device/realme/RMX2001/maintainer.png"
-
-	# let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-	  export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-	  export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-	  export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-	  export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+else
+	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
 	fi
 fi
+#
