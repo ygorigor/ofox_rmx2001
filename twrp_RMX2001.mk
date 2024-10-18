ea6a375b6f276a0104e666a66e25a043d163e0e8
Igor Sorocean <igor_sorocean@hotmail.com>
2024-10-18 12:09:49 +0300

    add gsi keys

--- twrp_RMX2001.mk
+++ twrp_RMX2001.mk
@@ -21,10 +21,11 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 
 # Inherit our device configuration
-$(call inherit-product, device/realme/RMX2001/device.mk)
+$(call inherit-product, device/realme/rmx2001/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
