0111edeff84afac356a3e712d56fe9107251c9f5
Igor Sorocean <igor_sorocean@hotmail.com>
2024-10-18 13:40:26 +0300

    update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,21 +1,21 @@
 #
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2021-2024 The OrangeFox Recovery Project
+#    This file is part of the OrangeFox Recovery Project
+#     Copyright (C) 2021-2024 The OrangeFox Recovery Project
 #
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
+#    OrangeFox is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
 #
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+#    OrangeFox is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 #
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
+#     This software is released under GPL version 3 or any later version.
+#    See <http://www.gnu.org/licenses/>.
 #
-# 	Please maintain this if you use this script or any part of it
+#     Please maintain this if you use this script or any part of it
 #
 FDEVICE="RMX2001"
 #set -o xtrace
@@ -35,30 +35,30 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-        export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v27.0.zip
-   	export TW_DEFAULT_LANGUAGE="en"
-	export LC_ALL="C"
- 	export ALLOW_MISSING_DEPENDENCIES=true
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_ENABLE_APP_MANAGER=1
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-        export FOX_DELETE_AROMAFM=1
-        export TARGET_DEVICE_ALT="RMX2001, RMX2151, wasabi"
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-        export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+    export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v28.0.zip
+    export TW_DEFAULT_LANGUAGE="en"
+    export LC_ALL="C"
+    export ALLOW_MISSING_DEPENDENCIES=true
+    export FOX_USE_NANO_EDITOR=1
+    export FOX_ENABLE_APP_MANAGER=1
+    export FOX_USE_BASH_SHELL=1
+    export FOX_ASH_IS_BASH=1
+    export FOX_USE_TAR_BINARY=1
+    export FOX_USE_SED_BINARY=1
+    export FOX_USE_XZ_UTILS=1
+    export FOX_DELETE_AROMAFM=1
+    export TARGET_DEVICE_ALT="RMX2001, RMX2151, wasabi"
+    export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+    export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
 
-	# R11.1 Settings
-	export FOX_VERSION="R11.1_7"
-	export OF_MAINTAINER="ygorigor"
+    # R11.1 Settings
+    export FOX_VERSION="R11.1_7"
+    export OF_MAINTAINER="ygorigor"
 
 else
-	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
-		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
-	fi
+    if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+        echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
+    fi
 fi
 #
