41aedf4b9a3b6ad9e2e9a12a3c0cf883c93b62f6
Igor Sorocean <igor_sorocean@hotmail.com>
2024-10-21 10:38:05 +0300

    remove FOX_VANILLA_BUILD

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -51,7 +51,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
     export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
     export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
-    export FOX_VANILLA_BUILD=1
     export OF_DEFAULT_KEYMASTER_VERSION=4.1
     export OF_DEFAULT_TIMEZONE="WET-2;WET"
 
